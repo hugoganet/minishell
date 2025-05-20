324654bacbc792d3d4a9b1fadbc47b7d174475d5
Elsa Audrezet <elaudrez@f6r4s15.clusters.42paris.fr>
2025-05-20 17:53:38 +0200

    Commit 1

---
+++ minishell.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elaudrez <elaudrez@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/20 17:38:44 by elaudrez          #+#    #+#             */
+/*   Updated: 2025/05/20 17:49:06 by elaudrez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+typedef struct s_list
+{
+	int	type;
+	char *str;
+	struct s_list	*next;
+} t_list;
+
+#endif

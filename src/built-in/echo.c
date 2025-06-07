6258fc3058cc130b891240157b3a3e1640a973bc
Elsa <teresa.luedaz@gmail.com>
2025-06-07 11:42:30 +0200

    fin taff maison

--- src/built-in/echo.c
+++ src/built-in/echo.c
@@ -6,9 +6,11 @@
 /*   By: bernard <bernard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 20:14:00 by bernard           #+#    #+#             */
-/*   Updated: 2025/06/04 20:18:17 by bernard          ###   ########.fr       */
+/*   Updated: 2025/06/06 13:03:34 by bernard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "minishell.h"
 
 Boucler sur ast, si node->type == CMD. 
 regarder si built in 
@@ -20,3 +22,18 @@ si echo
 node -> right ou node left 
 str -> si single quote, ecrire tout. 
 str -> 
+
+void    print_env(t_shell *data, t_ast *node)
+{
+    int i;
+
+    i = 0;
+    if (node->type == CMD && node->str == 'env')
+    {
+        while (data->env[i])
+        {
+            printf("%s\n", data->env[i]);
+            i++;
+        }
+    }
+}

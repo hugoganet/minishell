5161e44ff24b5ddca6653e8eebe2766ac420e649
Elsa Audrezet <elaudrez@f6r5s7.clusters.42paris.fr>
2025-06-24 11:50:05 +0200

    24/06

--- src/built-in/unset.c
+++ src/built-in/unset.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   unset.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bernard <bernard@student.42.fr>            +#+  +:+       +#+        */
+/*   By: elaudrez <elaudrez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 12:42:15 by bernard           #+#    #+#             */
-/*   Updated: 2025/06/18 12:04:01 by bernard          ###   ########.fr       */
+/*   Updated: 2025/06/23 17:35:32 by elaudrez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int ft_is_valid(char *args)
     int i;
 
     i = 0;
-    if (!args || (ft_is_alpha(key[0] && key[0] != '_')))
+    if (!args || (ft_is_alpha(args[0] && args[0] != '_')))
         return (0);
     while (args[i])
     {
@@ -34,23 +34,23 @@ void    remove_env_node(t_env **env, char *key)
     t_env   *prev;
 
     current = *env;
-    prv = NULL;
+    prev = NULL;
     while(current)
-    {
-        if(ft_strcmp(current->key, key) == 0)
-        {
-            if (prev == NULL)
-                *env = current->next;
-            else
-                prev->next = current->next;
-            free(current->value);
-            free(current->key);
-            free(current);
-            return ;
-        }
-        prev = current;
-        current = current->next;
-    }
+	{
+		if(ft_strcmp(current->key, key) == 0)
+		{
+			if (prev == NULL)
+				*env = current->next;
+			else
+				prev->next = current->next;
+			free(current->value);
+			free(current->key);
+			free(current);
+			return ;
+		}
+		prev = current;
+		current = current->next;
+	}
 }
 
 int ft_unset(t_env **env, t_ast *node)
@@ -63,7 +63,7 @@ int ft_unset(t_env **env, t_ast *node)
         if (ft_is_valid(node->args[i]))
             remove_env_node(*env, node->args[i]);
         else
-            printf("unset : %s : not a valid identifier\n", node->args[i])
+            printf("unset : %s : not a valid identifier\n", node->args[i]);
         i++;
     }
     return (0);

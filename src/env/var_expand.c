e86343444aa9859fb774bbd53fbd8b4e360624f8
Elsa Audrezet <elaudrez@f6r6s14.clusters.42paris.fr>
2025-06-27 16:13:35 +0200

    quotes marchent

--- src/env/var_expand.c
+++ src/env/var_expand.c
@@ -6,7 +6,7 @@
 /*   By: elaudrez <elaudrez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 19:54:08 by elaudrez          #+#    #+#             */
-/*   Updated: 2025/06/27 16:06:17 by elaudrez         ###   ########.fr       */
+/*   Updated: 2025/06/27 16:11:46 by elaudrez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -179,32 +179,13 @@ char *remove_quotes(char *str)
 	while (str[i])
 	{
 		// Toggle quote simple si hors quote double
-		if (str[i] == '\'' && in_double_quote == false && in_single_quote ==  false)
-		{
-			printf("On rentre dans des single\n");
-			in_single_quote = true;
-		}
-		else if (str[i] == '\'' && in_double_quote == false && in_single_quote == true)
-		{
-			printf("On sort des single\n");
-			in_single_quote = false;
-		}
+		if (str[i] == '\'' && !in_double_quote)
+			in_single_quote = !in_single_quote;
 		// Toggle quote double si hors quote simple
-		else if (str[i] == '"' && in_single_quote == false && in_double_quote == false)
-		{
-			printf("On rentre dans des double\n");
-			in_double_quote = true;	
-		}
-		else if (str[i] == '"' && in_single_quote == false && in_double_quote == true)
-		{
-			printf("On sort des double\n");
-			in_double_quote = false;
-		}
+		else if (str[i] == '"' && !in_single_quote)
+			in_double_quote = !in_double_quote;
 		else
-		{
-			result[j] = str[i];
-			j++;
-		}	
+			result[j++] = str[i];
 		i++;
 	}
 	return (result);

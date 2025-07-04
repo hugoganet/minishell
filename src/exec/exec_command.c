4b554cd696918084cb7bf122df6842dff05db18b
Elsa Audrezet <elaudrez@f6r8s14.clusters.42paris.fr>
2025-07-04 19:24:02 +0200

    dernier

--- src/exec/exec_command.c
+++ src/exec/exec_command.c
@@ -6,7 +6,7 @@
 /*   By: elaudrez <elaudrez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/04 08:32:17 by hugoganet         #+#    #+#             */
-/*   Updated: 2025/07/04 17:12:13 by elaudrez         ###   ########.fr       */
+/*   Updated: 2025/07/04 17:24:59 by elaudrez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -113,7 +113,7 @@ static int handle_child_status(int status)
 	// Si l'enfant a été tué par un signal
 	if (WIFSIGNALED(status))
 	{
-		if (WTERMSIG(status) == SIGINT)ishell>
+		if (WTERMSIG(status) == SIGINT)
 			write(STDOUT_FILENO, "\n", 1);
 		else if (WTERMSIG(status) == SIGQUIT)
 			write(STDOUT_FILENO, "Quit (core dumped)\n", 20);

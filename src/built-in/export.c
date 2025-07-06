143b055e9488f43fc207c3b0168ba164ce5b2697
Hugo Ganet <ganethugo@gmail.com>
2025-07-06 16:39:07 +0200

    feat(export) : continu de traiter les autres arguments même si le 1er est invalide

--- src/built-in/export.c
+++ src/built-in/export.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: elaudrez <elaudrez@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hugoganet <hugoganet@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/08 18:55:13 by elaudrez          #+#    #+#             */
-/*   Updated: 2025/07/04 16:47:03 by elaudrez         ###   ########.fr       */
+/*   Updated: 2025/07/06 16:38:31 by hugoganet        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	update_existing_env_value(t_env *env, char *key, char *value)
+int update_existing_env_value(t_env *env, char *key, char *value)
 {
 	while (env)
 	{
@@ -27,28 +27,26 @@ int	update_existing_env_value(t_env *env, char *key, char *value)
 	return (0);
 }
 
-static void	upadate_or_add_env(t_shell *data, char *key, char *value)
+static void upadate_or_add_env(t_shell *data, char *key, char *value)
 {
-	if (!update_existing_env_value(data->env_list, key, value)
-		&& ft_is_valid(key))
+	if (!update_existing_env_value(data->env_list, key, value) && ft_is_valid(key))
 		create_add_new_node(key, value, &data->env_list);
-	if (!update_existing_env_value(data->export_list, key, value)
-		&& ft_is_valid(key))
+	if (!update_existing_env_value(data->export_list, key, value) && ft_is_valid(key))
 	{
 		create_add_new_node(key, value, &data->export_list);
 		sort_list(&data->export_list);
 	}
 }
 
-static int	export_equal(char *arg, t_shell *data)
+static int export_equal(char *arg, t_shell *data)
 {
-	int		j;
-	char	*key;
-	char	*value;
+	int j;
+	char *key;
+	char *value;
 
 	j = 0;
 	while (arg[j] && arg[j] != '=')
-				j++;
+		j++;
 	key = ft_substr(arg, 0, j);
 	if (!ft_is_valid(key))
 	{
@@ -65,23 +63,32 @@ static int	export_equal(char *arg, t_shell *data)
 	return (0);
 }
 
-int	ft_export(t_ast *node, t_shell *data)
+int ft_export(t_ast *node, t_shell *data)
 {
-	int		i;
+	int i;
+	int error_status;
 
 	i = 1;
+	error_status = 0;
 	if (!node->args[1])
 		print_export_list(data->export_list);
 	while (node->args[i])
-	{	
+	{
 		if (!ft_strchr(node->args[i], '=') && ft_is_valid(node->args[i]))
-		{	
+		{
 			create_add_new_node(node->args[i], NULL, &data->export_list);
 			sort_list(&data->export_list);
 		}
+		else if (!ft_strchr(node->args[i], '=') && !ft_is_valid(node->args[i]))
+		{
+			ft_putstr_fd("Minishell: export: ", 2);
+			ft_putstr_fd(node->args[i], 2);
+			ft_putstr_fd(" not a valid identifier\n", 2);
+			error_status = 1;
+		}
 		else if (export_equal(node->args[i], data))
-			return (1);
+			error_status = 1;
 		i++;
 	}
-	return (0);
+	return (error_status);
 }
